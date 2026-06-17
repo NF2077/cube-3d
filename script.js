16fa170364300945135d6120fef4f3f9726df383
Jetsvk <141232374+NF2077@users.noreply.github.com>
2026-06-17 15:07:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,100 +1,520 @@
-function threejs() {
-	this.shapeX = 0.5;
-	this.shapeY = 100;
-	this.shapeZ = 100;
-	this.shapeColor = 0xffffff;
+
+let sceneCamera = document.querySelector("section[data-camera]");
+let scene = document.querySelector(".scene");
+
+
+function elem(id){
+	return document.getElementById(id);
 }
 
-threejs.prototype.init = function init() {
-	this.scene = new THREE.Scene();
-	this.camera();
-	this.renderer();
-	this.light();
-	this.floor();
 
-	this.initShape();
+var aCredit = document.getElementById("a-credit");
+var aMusic = document.getElementById("a-music1");
+var aLine = document.getElementById("a-line");
+var aRound = document.getElementById("a-round");
+var aGameOver = document.getElementById("a-gameover");
 
-	this.render();
-};
+function toggleSound() {
+  elem('sound').classList.toggle('off');
+  if(elem('sound').classList.contains('off'))
+	aLine.volume=aRound.volume=aGameOver.volume=aCredit.volume=0;
+  else
+    aLine.volume=aRound.volume=aGameOver.volume=aCredit.volume=1;
+}
 
-threejs.prototype.camera = function camera() {
-	this.camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 1000 );
-	this.camera.position.y = 500;
-	this.camera.position.z = 500;
-	this.camera.position.x = 500;
-	this.camera.updateProjectionMatrix();
-	this.camera.lookAt(this.scene.position);
-};
+function togglePlay() {
+  elem('music').classList.toggle('off');
+  return aMusic.paused ? aMusic.play() : aMusic.pause();
+}
 
-threejs.prototype.renderer = function renderer() {
-	this.renderer = new THREE.WebGLRenderer({antialias: true});
-	this.renderer.setSize( window.innerWidth, window.innerHeight );
-	this.renderer.setClearColor( 0x202020 , 1 );
-	this.renderer.shadowMapEnabled = true;
-	this.renderer.shadowMapType = THREE.PCFSoftShadowMap;
-	document.body.appendChild(this.renderer.domElement);
-};
+function toggleView() {
+  elem('content').classList.toggle('game-mode');
+  elem('modes').classList.toggle('game');
+	elem('center-scene').click();
+}
 
-threejs.prototype.light = function light() {
-	var shadowlight = new THREE.DirectionalLight( 0xffffff, 1.8 );
-	shadowlight.position.set( 0, 50, 0 );
-	shadowlight.castShadow = true;
-	shadowlight.shadowDarkness = 0.1;
-	this.scene.add(shadowlight);
+function toggleGlass() {
+  elem('glass').classList.toggle('hide');
+  elem('front-glass').classList.toggle('hide');
+}
 
-	var light = new THREE.DirectionalLight( 0xffffff, 1.8 );
-	light.position.set( 60, 100, 20 );
-	this.scene.add(light);
-  
-  var backLight = new THREE.DirectionalLight( 0xffffff, 1 );
-	backLight.position.set( -40, 100, 20 );
-	this.scene.add(backLight);
-};
+function toggleJoystick() {
+  elem('block-joystick').classList.toggle('blocking');
+  elem('controller').classList.toggle('layers');
+}
 
-threejs.prototype.floor = function floor() {
- 	var geometry = new THREE.PlaneGeometry( 500, 500, 1, 1 );
-	var material = new THREE.MeshBasicMaterial( { color: 0x202020 } );
-	this.floor = new THREE.Mesh( geometry, material );
-	this.floor.material.side = THREE.DoubleSide;
-	this.floor.position.y =-150;
-	this.floor.rotation.x = 90*Math.PI/180;
-	this.floor.rotation.y = 0;
-	this.floor.rotation.z = 0;
-	this.floor.doubleSided = true;
-    this.floor.receiveShadow = true;
-	this.scene.add(this.floor);
-};
+function centerScene() {
+	scene.style.left = 0;
+	scene.style.top = 0;
+	sceneCamera.style = "--scale:85";
+}
+
+function newCredit() {
+  aCredit.play();
+  setTimeout(function() {window.parent.location.href="https://codepen.io/josetxu/full/bGKqxyR";}, 250);
+}
+
+document.getElementById('reload').addEventListener('click',function(e){
+  e.preventDefault();
+  newCredit();
+});
 
 
-threejs.prototype.initShape = function initShape() {
-	this.myArray = new THREE.Group();
-	this.scene.add(this.myArray);
 
-		this.geometry = new THREE.BoxGeometry( 50, 50, 50 );
-		this.material = new THREE.MeshLambertMaterial({color : 0xF9F8ED, shading: THREE.FlatShading});
-		this.shape = new THREE.Mesh(this.geometry, this.material);
-		//this.shape.rotation.y = -Math.PI/4;
-		this.shape.castShadow = true;
-		this.shape.receiveShadow = false;
-		this.myArray.add(this.shape);
-		this.tl = new TimelineMax({repeat: -1 ,repeatDelay:0.5});
-		this.tl.to(this.shape.scale, 0.5, {x: 2, ease: Expo.easeOut});
-		this.tl.to(this.shape.scale, 0.5, {z: 2, ease: Expo.easeOut});
-		this.tl.to(this.shape.scale, 1, {y: 2, ease: Elastic.easeOut});
-		this.tl.to(this.shape.scale, 0.7, {z: 1,x:1,y:1, ease: Expo.easeOut});
-		this.tl.to(this.shape.rotation, 0.7, {y:-Math.PI, ease: Expo.easeOut},"=-0.7");
 
+
+countI = countO = countT = countS = countZ = countJ = countL = 0;
+function showStats(a,b) {
+	elem(a).style.height = window[b]+'px';
+}
+
+numLines = 0;
+function showLines() {
+	elem('lines').innerText= numLines;
+	elem('rainbow').style.height= numLines+'px';
 	
+	for(var i=1; i<11; i++){
+		if(numLines>i*10-1) { speed = 22 - i*2; elem('round').innerText=i+1; };
+		if(numLines % 10 === 0) { 
+			elem('round').classList.remove('showing'); 
+			setTimeout(function() {elem('round').classList.add('showing');}, 500);
+			aRound.play();	
+			//change song
+			if(!elem('music').classList.contains('off')){
+				var rnd = elem('round').innerText;
+				aMusic.pause();
+				//elem('a-music').src="";
+				if(rnd == 1 || rnd == 4 || rnd == 7){
+					aMusic = document.getElementById("a-music1");
+				} else if(rnd == 2 || rnd == 5 || rnd == 8){
+					aMusic = document.getElementById("a-music2");
+				} else {
+					aMusic = document.getElementById("a-music3");	
+				}
+				setTimeout(function() { aMusic.play(); }, 3000);
+				
+			}
+		}
+	}
+}
 
+score = 0; 
+function showScore() {
+	elem('score').innerText= score;
+}
+
+
+speed = 22;
+
+
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// generate a new tetromino sequence
+// @see https://tetris.fandom.com/wiki/Random_Generator
+function generateSequence() {
+  const sequence = ['I', 'J', 'L', 'O', 'S', 'T', 'Z'];
+
+  while (sequence.length) {
+    const rand = getRandomInt(0, sequence.length - 1);
+    const name = sequence.splice(rand, 3)[0]; /*1*/
+    tetrominoSequence.push(name);
+  }
+}
+
+// get the next tetromino in the sequence
+function getNextTetromino() {
+  if (tetrominoSequence.length === 0) {
+    generateSequence();
+  }
+
+  const name = tetrominoSequence.pop();
+  const matrix = tetrominos[name];
+
+  // I and O start centered, all others start in left-middle
+  const col = playfield[0].length / 2 - Math.ceil(matrix[0].length / 2);
+
+  // I starts on row 21 (-1), all others start on row 22 (-2)
+  const row = name === 'I' ? -1 : -2;
+
+  return {
+    name: name,      // name of the piece (L, O, etc.)
+    matrix: matrix,  // the current rotation matrix
+    row: row,        // current row (starts offscreen)
+    col: col         // current col
+  };
+}
+
+// rotate an NxN matrix 90deg
+// @see https://codereview.stackexchange.com/a/186834
+function rotate(matrix) {
+  const N = matrix.length - 1;
+  const result = matrix.map((row, i) =>
+    row.map((val, j) => matrix[N - j][i])
+  );
+
+  return result;
+}
+
+// check to see if the new matrix/row/col is valid
+function isValidMove(matrix, cellRow, cellCol) {
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      if (matrix[row][col] && (
+          // outside the game bounds
+          cellCol + col < 0 ||
+          cellCol + col >= playfield[0].length ||
+          cellRow + row >= playfield.length ||
+          // collides with another piece
+          playfield[cellRow + row][cellCol + col])
+        ) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+// place the tetromino on the playfield
+function placeTetromino() {
+  for (let row = 0; row < tetromino.matrix.length; row++) {
+    for (let col = 0; col < tetromino.matrix[row].length; col++) {
+      if (tetromino.matrix[row][col]) {
+
+        // game over if piece has any part offscreen
+        if (tetromino.row + row < 0) {
+          return showGameOver();
+        }
+		score++;
+		showScore();
+        playfield[tetromino.row + row][tetromino.col + col] = tetromino.name;
+		//console.log(tetromino.name);
+		
+		switch(tetromino.name) {
+			case 'I': countI++; break;
+			case 'O': countO++; break;
+			case 'T': countT++; break;
+			case 'S': countS++; break;
+			case 'Z': countZ++; break;
+			case 'J': countJ++; break;
+			case 'L': countL++; break;
+			default: ;
+		}
+		piece = 'stats'+tetromino.name
+		counter = 'count'+tetromino.name;
+		showStats('stats'+tetromino.name,'count'+tetromino.name);
+      }
+    }
+  }
+
+  // check for line clears starting from the bottom and working our way up
+  for (let row = playfield.length - 1; row >= 0; ) {
+    if (playfield[row].every(cell => !!cell)) {
+
+      // drop every row above this one
+      for (let r = row; r >= 0; r--) {
+        for (let c = 0; c < playfield[r].length; c++) {
+          playfield[r][c] = playfield[r-1][c];
+        }
+		score++;
+		showScore();
+      }
+	  numLines++;
+	  showLines();
+	  aLine.play();
+    }
+    else {
+      row--;
+    }
+  }
+
+  //tetromino = getNextTetromino();
+  
+  /*next*/
+  tetromino = tetrominoNext;
+  tetrominoNext = getNextTetromino();
+  console.log(tetrominoNext.name);
+  elem('nextpiece').className="next"+tetrominoNext.name;
+  /*next*/
+	
+}
+
+// show the game over screen
+function showGameOver() {
+  cancelAnimationFrame(rAF);
+  gameOver = true;
+  elem('gameover').style.display='block'; 
+  elem('insertcoin').classList.add('blink'); 
+  aMusic.volume = 0;
+  if(!elem('sound').classList.contains('off')){ aGameOver.play(); }
+}
+
+const canvas = elem('game');
+const context = canvas.getContext('2d');
+const grid = 22;
+const tetrominoSequence = [];
+
+// keep track of what is in every cell of the game using a 2d array
+// tetris playfield is 10x20, with a few rows offscreen
+const playfield = [];
+
+// populate the empty state
+for (let row = -2; row < 20; row++) {
+  playfield[row] = [];
+
+  for (let col = 0; col < 10; col++) {
+    playfield[row][col] = 0;
+  }
+}
+
+// how to draw each tetromino
+// @see https://tetris.fandom.com/wiki/SRS
+const tetrominos = {
+  'I': [
+    [0,0,0,0],
+    [1,1,1,1],
+    [0,0,0,0],
+    [0,0,0,0]
+  ],
+  'J': [
+    [1,0,0],
+    [1,1,1],
+    [0,0,0],
+  ],
+  'L': [
+    [0,0,1],
+    [1,1,1],
+    [0,0,0],
+  ],
+  'O': [
+    [1,1],
+    [1,1],
+  ],
+  'S': [
+    [0,1,1],
+    [1,1,0],
+    [0,0,0],
+  ],
+  'Z': [
+    [1,1,0],
+    [0,1,1],
+    [0,0,0],
+  ],
+  'T': [
+    [0,1,0],
+    [1,1,1],
+    [0,0,0],
+  ]
 };
 
-threejs.prototype.render = function render() {
-	requestAnimationFrame(this.render.bind(this));
-	this.renderer.render(this.scene, this.camera);
+// color of each tetromino
+const colors = {
+  'I': 'red',
+  'O': 'blue',
+  'T': 'green',
+  'S': 'cyan',
+  'Z': 'orange',
+  'J': 'yellow',
+  'L': 'purple'
 };
 
-var shape = new threejs();
-shape.init();
+let count = 0;
+//let tetromino = getNextTetromino();
+
+/*next*/
+let tetrominoNext = getNextTetromino();
+let tetromino     = getNextTetromino();
+/*next*/
+
+let rAF = null;  // keep track of the animation frame so we can cancel it
+let gameOver = false;
+
+pauseGame = false;
+// game loop
+function loop() {
+  rAF = requestAnimationFrame(loop);
+	if(pauseGame != true) {
+		context.clearRect(0,0,canvas.width,canvas.height);
+
+		// draw the playfield
+		for (let row = 0; row < 20; row++) {
+			for (let col = 0; col < 10; col++) {
+				if (playfield[row][col]) {
+					const name = playfield[row][col];
+					context.fillStyle = colors[name];
+
+					// drawing 1 px smaller than the grid creates a grid effect
+					context.fillRect(col * grid, row * grid, grid-1, grid-1);
+				}
+			}
+		}
+
+		// draw the active tetromino
+		if (tetromino) {
+
+			// tetromino falls every 35 frames
+			if (++count > speed) { //speed 25
+				tetromino.row++;
+				count = 0;
+
+				// place piece if it runs into anything
+				if (!isValidMove(tetromino.matrix, tetromino.row, tetromino.col)) {
+					tetromino.row--;
+					placeTetromino();
+				}
+			}
+
+			context.fillStyle = colors[tetromino.name];
+
+			for (let row = 0; row < tetromino.matrix.length; row++) {
+				for (let col = 0; col < tetromino.matrix[row].length; col++) {
+					if (tetromino.matrix[row][col]) {
+
+						// drawing 1 px smaller than the grid creates a grid effect
+						context.fillRect((tetromino.col + col) * grid, (tetromino.row + row) * grid, grid-1, grid-1);
+					}
+				}
+			}
+		}
+	} //pauseGame
+}
 
 
 
+
+
+// listen to keyboard events to move the active tetromino
+document.addEventListener('keydown', function(e) {
+  //if (gameOver) return;
+
+  //  left and right arrow keys  (move)
+  if (e.which === 37 || e.which === 39) {
+	// move joystick
+	if(!elem('block-joystick').classList.contains('blocking')) {
+		if (e.which === 37)	elem('joystick').className='m-left'; else elem('joystick').className='m-right';
+	}
+    
+	if (gameOver) return;
+	
+    const col = e.which === 37
+      ? tetromino.col - 1
+      : tetromino.col + 1;
+
+    if (isValidMove(tetromino.matrix, tetromino.row, col)) {
+      tetromino.col = col;
+    }
+  }
+
+  // up arrow key (rotate)
+  if (e.which === 38 || e.which === 32) {
+	//move button
+	if(!elem('block-joystick').classList.contains('blocking')) {
+		elem('btn').className='pressed';
+	}
+	
+	if (gameOver) return;
+  
+    const matrix = rotate(tetromino.matrix);
+    if (isValidMove(matrix, tetromino.row, tetromino.col)) {
+      tetromino.matrix = matrix;
+    }
+  }
+
+  // down arrow key (drop)
+  if(e.which === 40) {
+	//move joystick
+	if(!elem('block-joystick').classList.contains('blocking')) {
+		elem('joystick').className='m-down';
+	}
+  
+	if (gameOver) return;
+  
+    const row = tetromino.row + 1;
+
+    if (!isValidMove(tetromino.matrix, row, tetromino.col)) {
+      tetromino.row = row - 1;
+
+      placeTetromino();
+      return;
+    }
+
+    tetromino.row = row;
+	
+  }
+
+  // Esc arrow key (center)
+  if(e.which === 27) {
+    scene.style.left = 0;
+	scene.style.top = 0;
+	sceneCamera.style = "--scale:80";
+  }
+	
+	// Pause the Game
+	if(e.which === 80) {
+    if(pauseGame == false) {
+			pauseGame = true;
+		} else {
+			pauseGame = false;
+		}
+  }
+  
+});
+
+
+
+
+document.addEventListener('keyup', function(e) {
+	elem('joystick').className='';
+	elem('btn').className='';
+});
+
+
+
+// start the game
+rAF = requestAnimationFrame(loop);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*** CAMERA SYSTEM ***/
+
+window.addEventListener("load", () => {
+	new Camera()
+		.setOptimalPerspective()
+		.with({
+			debug: true,
+			zoom: {
+				range: [60, 200]
+			},			
+			rotate: {
+				speed: 1.2
+			}
+		})
+		.init();
+});
+
+/*** SET FOCUS ***/
+window.addEventListener("load", () => {
+	window.focus();
+});
+window.addEventListener("keydown", function(e) {
+    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false);
